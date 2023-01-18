213ace22a25114ccebf1c401da688329833526a1
cojemma <iam880401@gmail.com>
2023-01-18 10:09:04 +0800

    mysql init progress

--- database/mysql.go
+++ database/mysql.go
@@ -8,9 +8,29 @@ import (
 var mysqlDB *gorm.DB
 
 func init() {
-	mysqlDB, _ = gorm.Open(mysql.Open("root:db@tcp(db:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:db@tcp(db:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	mysqlDB = db
+}
+
+func connectMysql() error {
+	db, err := gorm.Open(mysql.Open("root:db@tcp(db:3306)/mysql?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	mysqlDB = db
+
+	return nil
 }
 
 func GetDB() *gorm.DB {
+	if mysqlDB == nil {
+		connectMysql()
+	}
+
 	return mysqlDB
 }
